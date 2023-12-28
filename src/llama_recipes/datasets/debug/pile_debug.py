22758aed8167a87a745c02ce360f3b8ed05f3f33
kojimano <nk654>
2023-12-29 07:47:25 +0900

    Pushing the initial attempt of dataloading.

---
+++ src/llama_recipes/datasets/debug/pile_debug.py
@@ -0,0 +1,339 @@
+import torch
+import multiprocessing
+import json
+from torch.utils.data import Dataset
+from transformers import PreTrainedTokenizer, AutoTokenizer
+from typing import List, Optional, Tuple, Type, Union
+from pathlib import Path
+from dataclasses import dataclass
+from tqdm import tqdm
+import os
+import struct
+from enum import Enum
+from functools import lru_cache
+import numpy as np
+import time
+from IPython import embed
+
+@dataclass
+class DataConfig:
+    train_data_path: str
+    context_size: int
+
+_INDEX_HEADER = b"MMIDIDX\x00\x00"
+
+class DType(Enum):
+    """The np data type Enum for writing/reading the MMapIndexedDataset indices
+    """
+
+    uint8 = 1
+    int8 = 2
+    int16 = 3
+    int32 = 4
+    int64 = 5
+    float64 = 6
+    float32 = 7
+    uint16 = 8
+
+    @classmethod
+    def code_from_dtype(cls, value: Type[np.number]) -> int:
+        """Get the code from the dtype
+
+        Args:
+            value (Type[np.number]): The dtype
+
+        Returns:
+            int: The code
+        """
+        return cls[value.__name__].value
+
+    @classmethod
+    def dtype_from_code(cls, value: int) -> Type[np.number]:
+        """Get the dtype from the code
+
+        Args:
+            value (int): The code
+
+        Returns:
+            Type[np.number]: The dtype
+        """
+        return getattr(np, cls(value).name)
+
+    @staticmethod
+    def size(key: Union[int, Type[np.number]]) -> int:
+        """Get the size of the dtype/code in bytes
+
+        Args:
+            key (Union[int, Type[np.number]]): The dtype or code
+
+        Raises:
+            ValueError: If the key is neither dtype nor integer code
+
+        Returns:
+            int: The size of the dtype/code in in bytes
+        """
+        if isinstance(key, int):
+            return DType.dtype_from_code(key)().itemsize
+        elif np.number in key.__mro__:
+            return key().itemsize
+        else:
+            raise ValueError
+
+    @staticmethod
+    def optimal_dtype(cardinality: Optional[int]) -> Type[np.number]:
+        """Get the dtype to use for an index of a certain cardinality
+
+        Args:
+            cardinality (Optional[int]): The number of elements to be indexed
+
+        Returns:
+            Type[np.number]: The dtype to use for the index
+        """
+        if cardinality is not None and cardinality < 65500:
+            return np.uint16
+        else:
+            return np.int32
+
+
+
+class _IndexReader(object):
+    """Object class to read the index (.idx) file
+
+    Args:
+        idx_path (str): The path to the index file
+
+        multimodal (bool): Whether the dataset is multimodal
+    """
+
+    def __init__(self, idx_path: str, multimodal: bool) -> None:
+
+        #log_single_rank(logger, logging.INFO, f"Load the {type(self).__name__} from {idx_path}")
+
+        with open(idx_path, "rb") as stream:
+            header = stream.read(9)
+            assert header == _INDEX_HEADER, f"bad header, cannot read: {idx_path}"
+
+            version = struct.unpack("<Q", stream.read(8))[0]
+            assert version == 1, f"bad version, cannot read: {idx_path}"
+
+            code = struct.unpack("<B", stream.read(1))[0]
+            self.dtype = DType.dtype_from_code(code)
+            self.dtype_size = DType.size(self.dtype)
+
+            self.sequence_count = struct.unpack("<Q", stream.read(8))[0]
+            self.document_count = struct.unpack("<Q", stream.read(8))[0]
+
+            offset = stream.tell()
+
+        self.bin_buffer_mmap = np.memmap(idx_path, mode="r", order="C")
+        self.bin_buffer = memoryview(self.bin_buffer_mmap)
+
+        #log_single_rank(logger, logging.INFO, f"\tExtract the sequence lengths")
+        t_beg = time.time()
+        self.sequence_lengths = np.frombuffer(
+            self.bin_buffer, dtype=np.int32, count=self.sequence_count, offset=offset
+        )
+        t_end = time.time()
+        #log_single_rank(logger, logging.DEBUG, f"\t> time elapsed: {t_end - t_beg:4f} seconds")
+
+        #log_single_rank(logger, logging.INFO, f"\tExtract the sequence pointers")
+        t_beg = time.time()
+        self.sequence_pointers = np.frombuffer(
+            self.bin_buffer,
+            dtype=np.int64,
+            count=self.sequence_count,
+            offset=offset + self.sequence_lengths.nbytes,
+        )
+        t_end = time.time()
+        #log_single_rank(logger, logging.DEBUG, f"\t> time elapsed: {t_end - t_beg:4f} seconds")
+
+        #log_single_rank(logger, logging.INFO, f"\tExtract the document indices")
+        t_beg = time.time()
+        self.document_indices = np.frombuffer(
+            self.bin_buffer,
+            dtype=np.int64,
+            count=self.document_count,
+            offset=offset + self.sequence_lengths.nbytes + self.sequence_pointers.nbytes,
+        )
+        t_end = time.time()
+        #log_single_rank(logger, logging.DEBUG, f"\t> time elapsed: {t_end - t_beg:4f} seconds")
+
+        self.sequence_modes = None
+        if multimodal:
+            #log_single_rank(logger, logging.INFO, f"\tExtract the sequence modes")
+            t_beg = time.time()
+            self.sequence_modes = np.frombuffer(
+                self.bin_buffer,
+                dtype=np.int8,
+                count=self.sequence_count,
+                offset=offset
+                + self.sequence_lengths.nbytes
+                + self.sequence_pointers.nbytes
+                + self.document_indices.nbytes,
+            )
+            t_end = time.time()
+            #log_single_rank(logger, logging.DEBUG, f"\t> time elapsed: {t_end - t_beg:4f} seconds")
+
+        assert self.sequence_lengths.shape[0] == len(self)
+        assert self.sequence_lengths.shape[0] == self.sequence_count
+        assert self.sequence_lengths.shape[0] == self.document_indices[-1]
+        print(f"> total number of documents: {self.document_indices.shape[0] - 1}")
+
+        # TODO: re-split documents so that each chunck end-up as a sequence of 4096 [is it okay to have ] 
+
+        #log_single_rank(logger, logging.INFO, f"> total number of sequences: {len(self)}")
+        #log_single_rank(
+        #    logger,
+        #    logging.INFO,
+        #    f"> total number of documents: {self.document_indices.shape[0] - 1}",
+        #)
+
+    def __del__(self) -> None:
+        """Clean up the object
+        """
+        self.bin_buffer_mmap._mmap.close()
+        del self.bin_buffer_mmap
+
+    def __len__(self) -> int:
+        """Return the length of the dataset
+
+        Returns:
+            int: The length of the dataset
+        """
+        return self.sequence_count
+
+    @lru_cache(maxsize=8)
+    def __getitem__(self, idx: int) -> Tuple[np.int32, np.int64, Optional[np.int8]]:
+        """Return the pointer, length, and mode at the index
+
+        Args:
+            idx (int): The index into the dataset
+
+        Returns:
+            Tuple[np.int32, np.int64, Optional[np.int8]]: The pointer, length and mode at
+            the index
+        """
+        return (
+            self.sequence_pointers[idx],
+            self.sequence_lengths[idx],
+            self.sequence_modes[idx] if self.sequence_modes is not None else None,
+        )
+
+
+class PILEDataset(Dataset):
+    def __init__(
+        self,
+        dataset_config,
+        tokenizer: PreTrainedTokenizer,
+        partition: str = "train",
+    ) -> None:
+        """
+        Code largely borrowed from https://github.com/NVIDIA/Megatron-LM/blob/main/megatron/core/datasets/indexed_dataset.py
+        """
+        # keys: alignment_score, instruction, input, output, lang_pair
+        self.data_file_path: str = (
+            dataset_config.train_data_path if partition == "train" else dataset_config.val_data_path
+        )
+
+        self.partition = partition
+        self.max_words: int = dataset_config.context_size
+        self.tokenizer: PreTrainedTokenizer = tokenizer
+        self.index = None
+        self.bin_buffer = None
+        self.bin_buffer_mmap = None
+        self.initialize(self.data_file_path)
+
+    def initialize(self, path_prefix: str) -> None:
+        """Initialize the dataset
+
+        This method is called by MMapIndexedDataset.__init__ during object creation and by
+        MMapIndexedDataset.__setstate__ during un-puckling
+
+        Args:
+            path_prefix (str): The index (.idx) and data (.bin) prefix
+        """
+        self.index = _IndexReader(self.data_file_path.replace(".bin",  ".idx"), False)
+        self.bin_buffer_mmap = np.memmap((self.data_file_path), mode="r", order="C")
+        self.bin_buffer = memoryview(self.bin_buffer_mmap)
+
+        # split the dataset into the chunck of self.max_words
+        if self.partition == "train":
+            self.batches: List[Tuple] = []  # List of (sequence_pointer, sequence_length, sequence_mode)
+            
+            # Assuming the calculation of total_bytes is correct as per your data structure
+            total_bytes = self.index[-1][0] + self.index[-1][1] * DType.size(self.index.dtype)
+            current_pointer = self.index[0][0]
+
+            while current_pointer < total_bytes:
+                increment = self.max_words * DType.size(self.index.dtype)
+                next_pointer = current_pointer + increment
+
+                # Calculate the number of words for this batch
+                num_words = min(self.max_words, (total_bytes - current_pointer) // DType.size(self.index.dtype))
+
+                # Create and append the batch
+                batch = (current_pointer, num_words, None)
+                self.batches.append(batch)
+
+                current_pointer = next_pointer
+
+    def __len__(self) -> int:
+        # TODO
+        """Return the length of the dataset i.e. the number of sequences in the index
+
+        Returns:
+            int: The length of the dataset
+        """
+        return len(self.batches) if self.partition == "train" else len(self.index)
+
+    def __getitem__(self, index: int) -> dict[str, torch.Tensor]:
+        IGNORE_INDEX = -100
+
+        if self.partition == "train":
+            sequence_pointer, sequence_length, sequence_mode = self.batches[index]
+        else: 
+            sequence_pointer, sequence_length, sequence_mode = self.index[index]
+            sequence_length = min(sequence_length, self.max_words)
+
+        encoded_text: list[int] = np.frombuffer(
+            self.bin_buffer,
+            dtype=self.index.dtype,
+            count=sequence_length,
+            offset=sequence_pointer,
+        )
+
+        # encoded_text.append(self.tokenizer.eos_token_id)  # TODO: check if we don't need this.
+        encoded_text = encoded_text.astype(np.int16)
+        encoded_text_tensor = torch.tensor(encoded_text)
+        padding_size: int = self.max_words - encoded_text_tensor.size(0)
+        if padding_size > 0:
+            padding = torch.zeros(padding_size, dtype=torch.long)
+            encoded_text_tensor = torch.cat((encoded_text_tensor, padding))
+        elif padding_size < 0:
+            assert(False) # this statement should not be called
+            encoded_text_tensor = encoded_text_tensor[:self.max_words]
+
+        input_ids: torch.Tensor = encoded_text_tensor
+        labels: torch.Tensor = torch.cat((encoded_text_tensor[1:], torch.tensor([IGNORE_INDEX])))
+
+        attention_mask: torch.Tensor = torch.ones(self.max_words, dtype=torch.long)
+        attention_mask[encoded_text_tensor == 0] = 0
+
+        return {
+            "input_ids": input_ids,
+            "labels": labels,
+            "attention_mask": attention_mask,
+        }
+
+if __name__ == "__main__":
+
+    dataset_config = DataConfig(
+        "/home/acf15317dw/kotoba-tech/kotobamba/3rdparty/Megatron-LM/pile-mamba-debug_text_document.bin",
+        4096,
+    )    
+    tokenizer = AutoTokenizer.from_pretrained(
+        "EleutherAI/gpt-neox-20b", use_fast=True)
+
+    dataset = PILEDataset(dataset_config, tokenizer)
+    dataset.__getitem__(1)
+    
